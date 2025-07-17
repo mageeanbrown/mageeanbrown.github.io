c5803c78b6d619da71e00414fb0e2b717fc6aeb4
mageeanbrown <86623423+mageeanbrown@users.noreply.github.com>
2025-07-17 00:56:09 +0000

    Deploying to gh-pages from @ mageeanbrown/mageeanbrown.github.io@f5e895eb589dc2c6cce57905369c7ac7cef25fa4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,9 +444,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-bicycle-framebuilding",
+          title: 'Bicycle Framebuilding',
+          description: "Designing and building a bike frame from scratch",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
