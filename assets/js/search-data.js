9331b7f6832abd89ed734375c40fb0bde478b9b5
mageeanbrown <86623423+mageeanbrown@users.noreply.github.com>
2025-08-05 02:44:45 +0000

    Deploying to gh-pages from @ mageeanbrown/mageeanbrown.github.io@56670f5a7d7b37a7117c8e74bc5dde47bb5bcb13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,8 +10,8 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "A growing collection of my projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
