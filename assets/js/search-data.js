a40907d3ddc750adc45b407dbab78fc994677337
mageeanbrown <86623423+mageeanbrown@users.noreply.github.com>
2025-08-05 02:28:57 +0000

    Deploying to gh-pages from @ mageeanbrown/mageeanbrown.github.io@92066e331ba4db7f2e8700bcad55a79f4a7979c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "This page is under construction!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
